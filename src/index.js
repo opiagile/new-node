6f0b912ad2cb457c00bacf848fb9110978dc25b8
Francisco Araujo <francisco@opiagile.com>
2020-09-09 22:26:06 -0300

    Implementação de tipos de parametros

--- src/index.js
+++ src/index.js
@@ -2,12 +2,26 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
+
 /*app.get('/',(request, response) => {
     return response.json({message : 'Hello GoStack'});
 });
 */
 
+
+/*
+    Query Paramns: Filtros e paginação
+    Route Paramns: Identificar recursos para atualizar ou deletar 
+    Request Body: conteudo para criar ou editar um recurso  
+*/
 app.get('/projects',(request, response) => {
+
+    const { title, owner} = request.query;
+    console.log(title);
+    console.log(owner);
+    
     return response.json([
         'Get Projeto 1',
         'Get Projeto 2',
@@ -15,13 +29,23 @@ app.get('/projects',(request, response) => {
 });
 
 app.post('/projects',(request, response) => {
+
+    const { title,owner } = request.body;
+    console.log(title);
+    console.log(owner);
+
     return response.json([
         'Post Projeto 1',
         'Post Projeto 2',
         'Post Projeto 3',
     ]);
+});
 
 app.put('/projects/:id',(request, response) => {
+
+    const { id } = request.params;
+    console.log(id);
+
     return response.json([
         'Put Projeto 1',
         'Put Projeto 2',
